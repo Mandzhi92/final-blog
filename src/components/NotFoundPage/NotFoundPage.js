de45f6c0e006329533fd8504696b7e7ff1cfd64c
Mandzhi92 <pavlovmandzhi@mail.ru>
2023-01-17 15:24:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import React from 'react'
+import { Link } from 'react-router-dom'
+import classes from './NotFoundPage.module.scss'
+
+const NotFoundPage = () => {
+  return (
+    <div className={classes.wrapper}>
+      Page not found :( 
+      <Link to="/">Go back to main page</Link>
+    </div>
+  )
+}
+
+export default NotFoundPage
